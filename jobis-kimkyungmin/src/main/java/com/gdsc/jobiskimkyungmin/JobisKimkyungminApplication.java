0e2dc93da6730a1d6dac562f22d2bc3f5f0d4f7d
하이 <kkm07133@naver.com>
2024-07-08 18:46:04 +0900

    start project

---
+++ jobis-kimkyungmin/src/main/java/com/gdsc/jobiskimkyungmin/JobisKimkyungminApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.jobiskimkyungmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobisKimkyungminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobisKimkyungminApplication.class, args);
+    }
+
+}
---
+++ jobis-kimkyungmin/src/test/java/com/gdsc/jobiskimkyungmin/JobisKimkyungminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.jobiskimkyungmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobisKimkyungminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
