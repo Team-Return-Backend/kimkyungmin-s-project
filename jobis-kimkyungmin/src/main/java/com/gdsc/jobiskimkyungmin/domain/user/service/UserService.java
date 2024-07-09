95cc3980ac5f9fb2638b2af807974514b0b052bd
하이 <kkm07133@naver.com>
2024-07-09 09:14:18 +0900

    UserService 추가(회원가입 기능)

---
+++ jobis-kimkyungmin/src/main/java/com/gdsc/jobiskimkyungmin/domain/user/service/UserService.java
@@ -0,0 +1,31 @@
+package com.gdsc.jobiskimkyungmin.domain.user.service;
+
+import com.gdsc.jobiskimkyungmin.domain.user.domain.Role;
+import com.gdsc.jobiskimkyungmin.domain.user.domain.User;
+import com.gdsc.jobiskimkyungmin.domain.user.domain.repository.UserRepository;
+import com.gdsc.jobiskimkyungmin.domain.user.dto.UserDto;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(UserDto userDto){ // 회원 가입 로직
+        if(userRepository.findByUsername(userDto.getUsername()).isPresent()){
+            throw new RuntimeException("User already exists");
+
+        }
+        User user =  User.builder()
+                .username(userDto.getUsername())
+                .password(userDto.getPassword())
+                .role(Role.BASIC)
+                .build();
+
+        return userRepository.save(user);
+    }
+}
